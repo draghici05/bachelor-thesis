3944d1a173d13f32973b9c783bf1b4c17485b212
draghici05 <alexdraghici190@gmail.com>
2025-03-04 17:01:16 +0200

    actually started working on this :D

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,3 @@
+export default function Navbar() {
+    return <h1>ASLE</h1>
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+// import { Container } from "react-dom";
+import Home from "./pages/HomePage/HomePage"
+// import Navbar from "./components/Navbar"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 )
